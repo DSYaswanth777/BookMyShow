819c01725f11b3731c1d7e6ae7c1075fd1cfc589
DSYaswanth777 <dsyaswanth777@gmail.com>
2021-08-30 12:29:42 +0530

    Installed Tailwind CSS and react

---
+++ src/layouts/Default.layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DefaultLayout = () =>{
+    return(
+        <div>
+            <h1 className="text-xl">Default Layout</h1>
+        </div>
+    )
+}
+export default DefaultLayout;
