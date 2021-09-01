acbc7157270245d48120f8c0a8eaa2328ec83890
DSYaswanth777 <dsyaswanth777@gmail.com>
2021-09-01 18:30:47 +0530

    Added TMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import axios from "axios";
 //Hoc 
 import DefaultHOC from "./HOC/Deafult.HOC";
 import MovieHOC from "./HOC/Movie.HOC";
@@ -9,13 +9,16 @@ import Plays from "./Pages/Plays.page";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+// axios default settings
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 function App() {
   return (
     <>
-<DefaultHOC path ="/" exact component={HomePage}/>
-<MovieHOC path ="/movie/:id" exact component={Movie}/>
-<DefaultHOC path ="/plays" exact component={Plays}/>
+      <DefaultHOC path="/" exact component={HomePage} />
+      <MovieHOC path="/movie/:id" exact component={Movie} />
+      <DefaultHOC path="/plays" exact component={Plays} />
     </>
   );
 }
--- src/components/HeroCarousal/HeroCarousal.component.jsx
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -1,69 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSlider from "react-slick";
-//Components
-import { NextArrow,PrevArrow } from "./Arrows.component";
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-const HeroCarousal = () => {
-    const settingsLG={
-        arrows:true,
-        centerMode:true,
-        centerPading:"200px",
-        slidesToShow:2,
-        infinte:1,
-        slidesToScroll:1,
-        nextArrow:<NextArrow/>,
-        PrevArrow:<PrevArrow/>,
-    }
-    const settings = {
-        arrows:true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow:<NextArrow/>,
-        PrevArrow:<PrevArrow/>,
-      };
+import axios from "axios";
 
-      const images =[
-         "https://images.unsplash.com/photo-1571780723643-4bfd5aa05ae3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-        "https://images.unsplash.com/photo-1570196428827-24a4140e7091?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8eGpQUjRobGtCR0F8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-        "https://images.unsplash.com/photo-1504649346668-2cc86afaa2e1?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8eGpQUjRobGtCR0F8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          "https://images.unsplash.com/photo-1570196428827-24a4140e7091?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8eGpQUjRobGtCR0F8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-         "https://images.unsplash.com/photo-1570196428827-24a4140e7091?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8eGpQUjRobGtCR0F8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-      ];
-    return (
-        <>
-        <div className="lg:hidden">
+// Compoenent
+import { NextArrow, PrevArrow } from "./Arrows.component";
+
+const HeroCarousal = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const requestNowPlayingMovies = async () => {
+      const getImages = await axios.get("/movie/now_playing");
+      setImages(getImages.data.results);
+    };
+
+    requestNowPlayingMovies();
+  }, []);
+
+  const settingsLG = {
+    arrows: true,
+    autoplay: true,
+    centerMode: true,
+    centerPadding: "300px",
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  const settings = {
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  return (
+    <>
+      <div className="lg:hidden">
         <HeroSlider {...settings}>
-            {images.map((image,id) => (
-                <div className="w-full h-56 md:h-80 py-3 " 
-                key ={id}>
-                    <img 
-                    src = {image} 
-                    alt ="testing" 
-                    className ="w-full h-full" 
-                    />
-                </div>
-            ))}
+          {images.map((image) => (
+            <div className="w-full h-56 md:h-80 py-3 ">
+              <img
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+                alt="testing"
+                className="w-full h-full"
+              />
+            </div>
+          ))}
         </HeroSlider>
-        </div>
-        <div className="hidden lg:block">
+      </div>
+
+      <div className="hidden lg:block">
         <HeroSlider {...settingsLG}>
-            {images.map((image,id) => (
-                <div className="w-20 h-96 px-2 py-3 rounded-md" 
-                key ={id}>
-                    <img 
-                    src = {image} 
-                    alt ="testing" 
-                    className ="w-full h-full" 
-                    />
-                </div>
-            ))}
+          {images.map((image) => (
+            <div className="w-full h-96 px-2 py-3">
+              <img
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+                alt="testing"
+                className="w-full h-full rounded-md"
+              />
+            </div>
+          ))}
         </HeroSlider>
-        </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
+
 export default HeroCarousal;
